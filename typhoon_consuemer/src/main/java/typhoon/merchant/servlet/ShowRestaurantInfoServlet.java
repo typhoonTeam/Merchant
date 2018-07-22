25d1a46c084ea5c5599ec968aa764d57de6b4875
JohnGao <john.gao@oocl.com>
2018-07-22 13:27:48 +0800

    完成查看申请信息界面的异步

--- typhoon_consuemer/src/main/java/typhoon/merchant/servlet/ShowRestaurantInfoServlet.java
+++ typhoon_consuemer/src/main/java/typhoon/merchant/servlet/ShowRestaurantInfoServlet.java
@@ -1,6 +1,7 @@
 package typhoon.merchant.servlet;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -8,9 +9,12 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import typhoon.merchant.pojo.RegisterInfo;
+import typhoon.merchant.pojo.Resturant;
 import typhoon.merchant.pojo.User;
 import typhoon.merchant.service.RegisterInfoService;
 import typhoon.merchant.service.impl.RegisterInfoServiceImpl;
+import typhoon.merchant.util.JsonParse;
+import typhoon.merchant.util.JsonParseByJackson;
 
 /**
  * Servlet implementation class ShowRestaurantInfoServlet
@@ -21,12 +25,23 @@ public class ShowRestaurantInfoServlet extends HttpServlet {
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		System.out.println("------------------------ShowRestaurantInfoServlet------------------------------");
 		User user = (User) request.getSession().getAttribute("user");
 
 		RegisterInfo registerInfo = registerInfoService.findRegisterInfoByShopId(user.getShopId());
 		request.setAttribute("registerInfo", registerInfo);
-		request.getRequestDispatcher("showRegisterInfo.jsp").forward(request, response);
+		
+		response.setContentType("text/html;charset=UTF-8");
+		response.setHeader("Pragma","No-cache");
+		response.setHeader("Cache-Control", "no-cache");
+		response.setDateHeader("Expires", -10);
+		PrintWriter out=response.getWriter();
+		JsonParse<Resturant> jsonUtil=new JsonParseByJackson<>();
+		String jsonData = jsonUtil.parseObjectToJson(registerInfo);
+	//	System.out.println(jsonData);
+		out.print(jsonData);
+		out.flush();
+		//request.getRequestDispatcher("showRegisterInfo.jsp").forward(request, response);
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
